358dc3a5d827b1cebfd0fcba86a5f95ac09c94d4
kyle-judd <juddkyle1408@gmail.com>
2021-04-14 16:04:46 -0400

    1.1

--- part1/part1/src/App.js
+++ part1/part1/src/App.js
@@ -1,4 +1,14 @@
-import logo from "./logo.svg";
+import React from "react";
+
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = (props) => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+);
+
+const Total = (props) => <p>Number of exercises {props.total}</p>;
 
 const App = () => {
   const course = "Half Stack application development";
@@ -8,20 +18,15 @@ const App = () => {
   const exercises2 = 7;
   const part3 = "State of a component";
   const exercises3 = 14;
+  const totalExercises = exercises1 + exercises2 + exercises3;
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total total={totalExercises} />
     </div>
   );
 };
