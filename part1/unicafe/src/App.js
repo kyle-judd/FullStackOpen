e685b359d837c22e065afd2a0f5497db01f9d4c3
kyle-judd <juddkyle1408@gmail.com>
2021-04-18 00:45:09 -0400

    1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,35 +1,81 @@
 import React, { useState, useEffect } from "react";
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [allReviews, setAllReviews] = useState([]);
-  const [average, setAverage] = useState(0);
+const Statistics = (props) => {
+  console.log();
+  return (
+    <div>
+      <h1>statistics</h1>
+      {Object.keys(props.stats).map((key, index) => {
+        if (key === "total" && props.stats.total.length !== 0) {
+          return (
+            <p key={index}>
+              {key} {props.stats[key].length}
+            </p>
+          );
+        } else {
+          return (
+            <p key={index}>
+              {key} {props.stats[key]}
+            </p>
+          );
+        }
+      })}
+    </div>
+  );
+};
 
-  useEffect(() => {
-    if (allReviews.length !== 0) calcAverage(allReviews);
+const App = () => {
+  const [stats, setStats] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: [],
+    average: 0,
+    positive: 0,
   });
 
-  const calcAverage = (arr) => {
-    const average = arr.reduce((a, b) => a + b) / arr.length;
-    setAverage(average);
+  useEffect(() => calcPercentages(), [stats.total]);
+
+  const calcPercentages = () => {
+    if (stats.total.length !== 0) {
+      const average = stats.total.reduce((a, b) => a + b) / stats.total.length;
+      const percentPositive =
+        (stats.total.filter((score) => score === 1).reduce((a, b) => a + b) /
+          stats.total.length) *
+        100;
+      setStats({ ...stats, average: average, positive: percentPositive });
+    }
   };
 
   const handleGoodClick = () => {
-    setGood(good + 1);
-    setAllReviews((allReviews) => [...allReviews, 1]);
+    setStats({
+      ...stats,
+      good: stats.good + 1,
+      total: [...stats.total, 1],
+    });
   };
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
-    setAllReviews((allReviews) => [...allReviews, 0]);
+    setStats({
+      ...stats,
+      neutral: stats.neutral + 1,
+      total: [...stats.total, 0],
+    });
   };
 
   const handleBadClick = () => {
-    setBad(bad + 1);
-    setAllReviews((allReviews) => [...allReviews, -1]);
+    setStats({
+      ...stats,
+      bad: stats.bad + 1,
+      total: [...stats.total, -1],
+    });
   };
+
+  let feedback = <p>No feedback given</p>;
+
+  if (stats.total.length !== 0) {
+    feedback = <Statistics stats={stats} />;
+  }
 
   return (
     <div>
@@ -37,12 +83,7 @@ const App = () => {
       <button onClick={handleGoodClick}>good</button>
       <button onClick={handleNeutralClick}>neutral</button>
       <button onClick={handleBadClick}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {allReviews.length}</p>
-      <p>average {average}%</p>
+      {feedback}
     </div>
   );
 };
