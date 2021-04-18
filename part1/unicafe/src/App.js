c561e2283e9251fe2983af672c7d88b4b229595e
kyle-judd <juddkyle1408@gmail.com>
2021-04-17 21:30:52 -0400

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allReviews, setAllReviews] = useState([]);
+  const [average, setAverage] = useState(0);
 
-  const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setNeutral(neutral + 1);
-  const handleBadClick = () => setBad(bad + 1);
+  useEffect(() => {
+    if (allReviews.length !== 0) calcAverage(allReviews);
+  });
+
+  const calcAverage = (arr) => {
+    const average = arr.reduce((a, b) => a + b) / arr.length;
+    setAverage(average);
+  };
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setAllReviews((allReviews) => [...allReviews, 1]);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setAllReviews((allReviews) => [...allReviews, 0]);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setAllReviews((allReviews) => [...allReviews, -1]);
+  };
 
   return (
     <div>
@@ -19,6 +41,8 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {allReviews.length}</p>
+      <p>average {average}%</p>
     </div>
   );
 };
