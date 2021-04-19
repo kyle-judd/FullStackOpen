c57c840ac29165de7582237f7ea9d3016b4acb9b
kyle-judd <juddkyle1408@gmail.com>
2021-04-19 10:29:05 -0400

    2.3

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -16,10 +16,7 @@ const Header = ({ course }) => {
 };
 
 const Total = ({ course }) => {
-  const sum =
-    course.parts[0].exercises +
-    course.parts[1].exercises +
-    course.parts[2].exercises;
+  const sum = course.parts.reduce((a, part) => a + part.exercises, 0);
   return <p>Number of exercises {sum}</p>;
 };
 
