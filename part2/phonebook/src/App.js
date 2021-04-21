2a3777b2bd6b1941fbe4f3216b08920dd648b212
kyle-judd <juddkyle1408@gmail.com>
2021-04-21 10:35:07 -0400

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
   const [newPhoneNumber, setNewPhoneNumber] = useState("");
+  const [query, setQuery] = useState("");
 
   const onNameChangeHandler = (event) => {
     setNewName(event.target.value);
@@ -11,6 +17,10 @@ const App = () => {
 
   const onPhoneNumberChangeHandler = (event) => {
     setNewPhoneNumber(event.target.value);
+  };
+
+  const onQueryChangeHandler = (event) => {
+    setQuery(event.target.value);
   };
 
   const onSubmitHandler = (event) => {
@@ -25,6 +35,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input onChange={onQueryChangeHandler} />
+      </div>
+      <h1>add a new</h1>
       <form onSubmit={onSubmitHandler}>
         <div>
           name: <input onChange={onNameChangeHandler} />
@@ -37,11 +51,19 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>
-          {person.name} {person.number}
-        </p>
-      ))}
+      {persons.map((person) => {
+        if (
+          person.name.toLowerCase().includes(query.toLowerCase()) ||
+          query === ""
+        ) {
+          return (
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          );
+        }
+        return;
+      })}
     </div>
   );
 };
