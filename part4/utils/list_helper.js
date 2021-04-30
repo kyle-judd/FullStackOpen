6ecac0671adbc2bd17aa0578ebb51c719ef0f3e8
kyle-judd <juddkyle1408@gmail.com>
2021-04-30 08:57:49 -0400

    4.6 and 4.7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,59 @@
 const dummy = (blogs) => 1;
 
-const totalLikes = (blogs) => blogs.reduce((sum, blog) => sum + blog.likes, 0);
+const totalLikes = (blogs) =>
+  blogs.length === 1
+    ? blogs[0].likes
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  if (!blogs) {
+    return undefined;
+  } else if (blogs.length === 1) {
+    return blogs[0];
+  } else {
+    return blogs.reduce(
+      (prev, current) => (prev.likes > current.likes ? prev : current),
+      1
+    );
+  }
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  } else {
+    let authorCounts = blogs.reduce((authorCount, blog) => {
+      authorCount[blog.author] = (authorCount[blog.author] || 0) + 1;
+      return authorCount;
+    }, {});
+    let maxCount = Math.max(...Object.values(authorCounts));
+    let mostFrequent = Object.keys(authorCounts).filter(
+      (author) => authorCounts[author] === maxCount
+    );
+    return {
+      author: mostFrequent[0],
+      blogs: maxCount,
+    };
+  }
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  } else {
+    let likesCounts = blogs.reduce((likesCount, blog) => {
+      likesCount[blog.author] = (likesCount[blog.author] || 0) + blog.likes;
+      return likesCount;
+    }, {});
+    let maxCount = Math.max(...Object.values(likesCounts));
+    let mostLiked = Object.keys(likesCounts).filter(
+      (author) => likesCounts[author] === maxCount
+    );
+    return {
+      author: mostLiked[0],
+      likes: maxCount,
+    };
+  }
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes };
